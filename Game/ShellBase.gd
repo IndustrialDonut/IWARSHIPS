0244c06f564afde71dbc99678d57b423ff1fd0d6
IndustrialDonut <76419762+IndustrialDonut@users.noreply.github.com>
2021-09-26 20:12:41 -0700

    before Propulsion system

--- Game/ShellBase.gd
+++ Game/ShellBase.gd
@@ -3,6 +3,6 @@ extends RigidBody
 
 
 func _physics_process(delta):
-	$MeshInstance.look_at(linear_velocity, Vector3.UP)
+	$MeshInstance.look_at(linear_velocity + global_transform.origin, Vector3.UP)
 	if self.global_transform.origin.y < -5:
 		self.queue_free()
--- Game/Ships/Weapons.gd
+++ Game/Ships/Weapons.gd
@@ -5,13 +5,12 @@ export(NodePath) onready var shipbase = get_node(shipbase)
 
 var selected_weapon : int = -1 # start with nothing selected
 
+var muzzle_velocity = 800 # strictly the horizontal velocity, in m/s
+var _muzzle_velocity = muzzle_velocity * CONSTANTS.UNITS_PER_METER
+
 var torp_salvo_size = 3
 
 var _salvo_counter = 0
-
-
-func select(weapon_enum : int):
-	selected_weapon = weapon_enum
 
 
 func _process(delta: float) -> void:
@@ -19,7 +18,10 @@ func _process(delta: float) -> void:
 	global_transform.origin = shipbase.global_transform.origin
 	
 	if Input.is_action_just_pressed("select_torpedo"):
-		select(CONSTANTS.ARM.TORP)
+		_select(CONSTANTS.ARM.TORP)
+
+	if Input.is_action_just_pressed("fire"):
+		_fire()
 	
 	var cam = get_viewport().get_camera()
 	var cam_forward = - cam.global_transform.basis.z * 1000 
@@ -35,16 +37,19 @@ func _process(delta: float) -> void:
 	else:
 		$Torps.rotate(- Vector3.UP, delta * 2)
 	
-	if Input.is_action_just_pressed("fire"):
-		fire()
+
+func _select(weapon_enum : int):
+	selected_weapon = weapon_enum
 
 
-func fire() -> void:
+func _fire() -> void:
 	
 	if selected_weapon == CONSTANTS.ARM.TORP:
 		_launch_torp()
 		_salvo_counter += 1
 		$Torps/Salvo.start()
+	else:
+		_fire_shell()
 
 
 func _on_Salvo_timeout() -> void:
@@ -55,7 +60,24 @@ func _on_Salvo_timeout() -> void:
 		_salvo_counter = 0
 		$Torps/Salvo.stop()
 
+
 func _launch_torp() -> void:
 	var torp = preload("res://Ships/Torpedo_0.tscn").instance()
 	owner.add_child(torp)
 	torp.setup($Torps.global_transform)
+
+
+func _fire_shell():
+	var shell = preload("res://Shell203JP.tscn").instance()
+	owner.add_child(shell)
+	shell.global_transform = $Torps.global_transform
+	shell.linear_velocity = _muzzle_velocity * -shell.global_transform.basis.z
+	var h_time_half = (_current_target_distance / _muzzle_velocity) / 2.0
+	var v0 = h_time_half * 9.8
+	
+	shell.linear_velocity.y += v0
+
+
+var _current_target_distance = 0
+func _on_ShipController_distance(dist) -> void:
+	_current_target_distance = dist
